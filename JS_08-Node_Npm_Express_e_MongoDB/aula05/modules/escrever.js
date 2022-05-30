c1f5836dd4e0ac470d18ae04c0cdba5576761693
geisondossantos <geison_bf@hotmail.com>
2022-05-29 22:23:12 -0300

    Atualização 29-05-2022

--- JS_08-Node_Npm_Express_e_MongoDB/aula05/modules/escrever.js
+++ JS_08-Node_Npm_Express_e_MongoDB/aula05/modules/escrever.js
@@ -1,14 +1,5 @@
 const fs = require("fs").promises;
-const path = require("path");
-const caminhoArquivo = path.resolve(__dirname, "..", "teste.json");
 
-const pessoas = [
-    { nome: "Geison" },
-    { nome: "Marcia" },
-    { nome: "Bernardo" },
-    { nome: "Kalel" },
-    { nome: "Zoe" },
-]
-const json = JSON.stringify(pessoas, "", 2);
-
-fs.writeFile(caminhoArquivo, json, { flag: "w" });
+module.exports = (caminho, dados) => {
+    fs.writeFile(caminho, dados, { flag: "w" })
+};
