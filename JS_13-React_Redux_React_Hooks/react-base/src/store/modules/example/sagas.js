67f5e50115ace12be3066abb395df3046c32db8d
geisondossantos <geison_bf@hotmail.com>
2022-06-15 11:21:38 -0300

    Adiciona redux persist

--- JS_13-React_Redux_React_Hooks/react-base/src/store/modules/example/sagas.js
+++ JS_13-React_Redux_React_Hooks/react-base/src/store/modules/example/sagas.js
@@ -4,10 +4,10 @@ import * as actions from './action';
 import * as types from '../types';
 
 const requisicao = () =>
-  new Promise((resolve, reject) => {
+  new Promise((resolve) => {
     setTimeout(() => {
-      reject();
-    }, 2000);
+      resolve();
+    }, 600);
   });
 
 function* exampleRequest() {
