c9742b689be99e6603c9c938ff72bbc45eb286d0
geisondossantos <geison_bf@hotmail.com>
2022-06-15 10:29:36 -0300

    Adiciona Redux Saga

--- JS_13-React_Redux_React_Hooks/react-base/src/store/modules/example/reducer.js
+++ JS_13-React_Redux_React_Hooks/react-base/src/store/modules/example/reducer.js
@@ -1,3 +1,5 @@
+import * as types from '../types';
+
 const initialState = {
   botaoClicado: false,
 };
@@ -5,11 +7,21 @@ const initialState = {
 // eslint-disable-next-line func-names
 export default function (state = initialState, action) {
   switch (action.type) {
-    case 'BOTAO_CLICADO': {
+    case types.BOTAO_CLICADO_SUCCESS: {
+      console.log('Sucesso');
       const newState = { ...state };
       newState.botaoClicado = !newState.botaoClicado;
       return newState;
     }
+    case types.BOTAO_CLICADO_FAILURE: {
+      console.log('Deu erro');
+      return state;
+    }
+    case types.BOTAO_CLICADO_REQUEST: {
+      console.log('Esto fazendo a requisição');
+      return state;
+    }
+
     default: {
       return state;
     }
